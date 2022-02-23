33dcd7bac60d672a8633d0cbd85ee91ae64372ae
Juan Alcantara <40488349@live.napier.ac.uk>
2022-02-23 13:51:48 +0000

    Input report type added

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,15 +1,116 @@
 package com.napier.sem;
 
+
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoCollection;
 import org.bson.Document;
 
+import java.util.Locale;
+import java.util.Scanner;
 
+/**
+ * This app will ask the user for the report type he wants and will then communicate with the database part of the project
+ *
+ * User will have to select a report type and the number selected will be send to another class that will get in return the requiered swl script
+ *
+ *
+ */
 public class App
 {
     public static void main(String[] args)
     {
+        Scanner scanner = new Scanner(System.in);
+        String territory;
+        int topFilter;
+
+        System.out.println("""
+                1) All the countries in the world organised by largest population to smallest
+                2) All the countries in a continent organised by largest population to smallest
+                3) All the countries in a region organised by largest population to smallest
+                4) The top N populated countries in the world where N is provided by the user
+                5) The top N populated countries in a continent where N is provided by the user
+                6) The top N populated countries in a region where N is provided by the user
+                7) All the cities in the world organised by largest population to smallest
+                8) All the cities in a continent organised by largest population to smallest
+                9) All the cities in a region organised by largest population to smallest
+                10) All the cities in a country organised by largest population to smallest
+                11) All the cities in a district organised by largest population to smallest
+                12) The top N populated cities in the world where N is provided by the user
+                13) The top N populated cities in a continent where N is provided by the user
+                14) The top N populated cities in a region where N is provided by the user
+                15) The top N populated cities in a country where N is provided by the user
+                16) The top N populated cities in a district where N is provided by the user
+                17) All the capital cities in the world organised by largest population to smallest
+                18) All the capital cities in a continent organised by largest population to smallest
+                19) All the capital cities in a region organised by largest to smallest
+                20) The top N populated capital cities in the world where N is provided by the user
+                21) The top N populated capital cities in a continent where N is provided by the user
+                22) The top N populated capital cities in a region where N is provided by the user
+                23) The population of people, people living in cities, and people not living in cities in each continent
+                24) The population of people, people living in cities, and people not living in cities in each region
+                25) The population of people, people living in cities, and people not living in cities in each country
+                26) Additional information reports
+                Enter the report number here:""");
+        int a = scanner.nextInt();
+        if (a == 26){
+
+            System.out.println("""
+                    1) Population of the world
+                    2) Population of a continent
+                    3) Population of a region
+                    4) Population of a country
+                    5) Population of a district
+                    6) Population od a city
+                    7) Language report
+                    Enter the report number here:""");
+            a = 30 + scanner.nextInt();
+        }
+        if ( a == 1|| a == 4|| a == 7|| a == 12|| a == 17|| a == 20|| a == 31|| a == 37){
+            territory = "WORLD";
+        }
+        else if ( a == 2|| a == 5|| a == 8|| a == 13|| a == 18|| a == 21|| a == 23|| a == 32){
+            System.out.println("Enter the name of the continent: ");
+            territory = scanner.next().toUpperCase(Locale.ROOT);
+        }
+        else if ( a == 3|| a == 6|| a == 9|| a == 14|| a == 19|| a == 22|| a == 24|| a == 33){
+            System.out.println("Enter the name of the region: ");
+            territory = scanner.next().toUpperCase(Locale.ROOT);
+        }
+        else if ( a == 10|| a == 15|| a == 25|| a == 34){
+            System.out.println("Enter the name of the country: ");
+            territory = scanner.next().toUpperCase(Locale.ROOT);
+        }
+        else if ( a == 11|| a == 16|| a == 35){
+            System.out.println("Enter the name of the district: ");
+            territory = scanner.next().toUpperCase(Locale.ROOT);
+        }
+        else if ( a == 36){
+            System.out.println("Enter the name of the city: ");
+            territory = scanner.next().toUpperCase(Locale.ROOT);
+        }
+        else{
+            System.out.println("wrong number input...");
+            territory = "no territory";
+        }
+
+        if (a == 4|| a == 5|| a == 6|| a == 12| a == 2613|| a == 14|| a == 15|| a == 16|| a == 20|| a == 21|| a == 22){
+            System.out.println("How many results from the top would you like to get: ");
+            topFilter = scanner.nextInt();
+        }
+        else{
+            topFilter = 0;
+        }
+
+        System.out.println("The report number: " + a + ", will be displayed about territory: " + territory + ", filtering the top: " + topFilter + " results.");
+
+
+
+
+
+
+
+        /*
         // Connect to MongoDB
         MongoClient mongoClient = new MongoClient("mongo-dbserver");
         // Get a database - will create when we use it
@@ -27,5 +128,7 @@ public class App
         // Check document in collection
         Document myDoc = collection.find().first();
         System.out.println(myDoc.toJson());
+
+         */
     }
 }
