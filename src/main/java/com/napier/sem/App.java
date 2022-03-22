631bc3109919d6406d17f342ad17232bd85689b1
Juan Alcantara <40488349@live.napier.ac.uk>
2022-03-22 18:58:42 +0000

    Simple validation added

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -27,7 +27,9 @@ public class App
     {
         Scanner scanner = new Scanner(System.in);
         String territory;
-        int topFilter;
+        int topFilter = 0;
+        int a=0;
+        boolean validInput=false;
 
         // Create new Application
         App app = new App();
@@ -35,9 +37,9 @@ public class App
         // Connect to database
         app.connect();
         // Get Employee
-        Report emp = app.getReport(255530);
+        //Report emp = app.getReport(255530);
         // Display results
-        app.displayReport(emp);
+        //app.displayReport(emp);
         // Disconnect from database
         app.disconnect();
 
@@ -45,51 +47,67 @@ public class App
 
         //TODO: Add validation for inputs(easy)
         //Ask user to decide between all reports
-        System.out.println("""
-                1) All the countries in the world organised by largest population to smallest
-                2) All the countries in a continent organised by largest population to smallest
-                3) All the countries in a region organised by largest population to smallest
-                4) The top N populated countries in the world where N is provided by the user
-                5) The top N populated countries in a continent where N is provided by the user
-                6) The top N populated countries in a region where N is provided by the user
-                7) All the cities in the world organised by largest population to smallest
-                8) All the cities in a continent organised by largest population to smallest
-                9) All the cities in a region organised by largest population to smallest
-                10) All the cities in a country organised by largest population to smallest
-                11) All the cities in a district organised by largest population to smallest
-                12) The top N populated cities in the world where N is provided by the user
-                13) The top N populated cities in a continent where N is provided by the user
-                14) The top N populated cities in a region where N is provided by the user
-                15) The top N populated cities in a country where N is provided by the user
-                16) The top N populated cities in a district where N is provided by the user
-                17) All the capital cities in the world organised by largest population to smallest
-                18) All the capital cities in a continent organised by largest population to smallest
-                19) All the capital cities in a region organised by largest to smallest
-                20) The top N populated capital cities in the world where N is provided by the user
-                21) The top N populated capital cities in a continent where N is provided by the user
-                22) The top N populated capital cities in a region where N is provided by the user
-                23) The population of people, people living in cities, and people not living in cities in each continent
-                24) The population of people, people living in cities, and people not living in cities in each region
-                25) The population of people, people living in cities, and people not living in cities in each country
-                26) Additional information reports
-                Enter the report number here:""");
-        int a = scanner.nextInt();
-
-        //Ask again but for special reports
-        if (a == 26){
-
+        while(!validInput) {
             System.out.println("""
-                    1) Population of the world
-                    2) Population of a continent
-                    3) Population of a region
-                    4) Population of a country
-                    5) Population of a district
-                    6) Population od a city
-                    7) Language report
+                    1) All the countries in the world organised by largest population to smallest
+                    2) All the countries in a continent organised by largest population to smallest
+                    3) All the countries in a region organised by largest population to smallest
+                    4) The top N populated countries in the world where N is provided by the user
+                    5) The top N populated countries in a continent where N is provided by the user
+                    6) The top N populated countries in a region where N is provided by the user
+                    7) All the cities in the world organised by largest population to smallest
+                    8) All the cities in a continent organised by largest population to smallest
+                    9) All the cities in a region organised by largest population to smallest
+                    10) All the cities in a country organised by largest population to smallest
+                    11) All the cities in a district organised by largest population to smallest
+                    12) The top N populated cities in the world where N is provided by the user
+                    13) The top N populated cities in a continent where N is provided by the user
+                    14) The top N populated cities in a region where N is provided by the user
+                    15) The top N populated cities in a country where N is provided by the user
+                    16) The top N populated cities in a district where N is provided by the user
+                    17) All the capital cities in the world organised by largest population to smallest
+                    18) All the capital cities in a continent organised by largest population to smallest
+                    19) All the capital cities in a region organised by largest to smallest
+                    20) The top N populated capital cities in the world where N is provided by the user
+                    21) The top N populated capital cities in a continent where N is provided by the user
+                    22) The top N populated capital cities in a region where N is provided by the user
+                    23) The population of people, people living in cities, and people not living in cities in each continent
+                    24) The population of people, people living in cities, and people not living in cities in each region
+                    25) The population of people, people living in cities, and people not living in cities in each country
+                    26) Additional information reports
                     Enter the report number here:""");
-            a = 30 + scanner.nextInt();
+            a = scanner.nextInt();
+            if (a < 1 || a > 26) {
+                System.out.println("WRONG INPUT TRY AGAIN");
+            } else {
+                validInput = true;
+            }
+        }
+        //Ask again but for special reports
+        validInput = false;
+        if (a == 26) {
+            while (!validInput) {
+                System.out.println("""
+                        1) Population of the world
+                        2) Population of a continent
+                        3) Population of a region
+                        4) Population of a country
+                        5) Population of a district
+                        6) Population od a city
+                        7) Language report
+                        Enter the report number here:""");
+                //numbers for reports above are from 31 to 37
+                a = 30 + scanner.nextInt();
+                if (a < 31|| a > 37){
+                    System.out.println("WRONG INPUT PLEASE TRY AGAIN");
+                }
+                else{
+                    validInput = true;
+                }
+            }
         }
 
+        //TODO if possible city validation within database
         //get the territory type specification
         if ( a == 1|| a == 4|| a == 7|| a == 12|| a == 17|| a == 20|| a == 31|| a == 37){
             territory = "WORLD";
@@ -121,9 +139,15 @@ public class App
 
         //If report requires a number for top results asks for one, else number will be 0
         //TODO: 0 must be recognised as no top filter in the code that gets this information
-        if (a == 4|| a == 5|| a == 6|| a == 12| a == 2613|| a == 14|| a == 15|| a == 16|| a == 20|| a == 21|| a == 22){
-            System.out.println("How many results from the top would you like to get: ");
-            topFilter = scanner.nextInt();
+        validInput = false;
+        if (a == 4|| a == 5|| a == 6|| a == 12| a == 13|| a == 14|| a == 15|| a == 16|| a == 20|| a == 21|| a == 22){
+            while(!validInput) {
+                System.out.println("How many results from the top would you like to get(number > 0): ");
+                topFilter = scanner.nextInt();
+                if(topFilter>0){
+                    validInput = true;
+                }
+            }
         }
         else{
             topFilter = 0;
@@ -132,23 +156,24 @@ public class App
         //Quick system out to ckeck it is working
         System.out.println("The report number: " + a + ", will be displayed about territory: " + territory + ", filtering the top: " + topFilter + " results.");
 
-
+        //TODO call functions to generate report
 
 
 
     }
 
-    public void displayReport(Report emp)
+    public void displayReport(Report report)
     {
-        if (emp != null)
+        if (report != null)
         {
             System.out.println("something");
             //display report
         }
     }
 
-    public Report getReport(int ID)
+    public Report getReport(int ID, String territory,int topFilter )
     {
+        //TODO implement correct script creation OR generate reports and save them in organised way
         try
         {
             // Create an SQL statement
