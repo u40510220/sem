95f59b01d2e2fa028349719690f3f6210c572bc9
VmUser4 <40510220@live.napier.ac.uk>
2022-03-21 23:30:27 -0700

    add mysql

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -8,6 +8,7 @@ package com.napier.sem;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -26,17 +27,61 @@ public class App
         String territory;
         int topFilter;
 
-        try {
+        //Connect to Database - Start
+        try
+        {
+            // Load Database driver
             Class.forName("com.mysql.cj.jdbc.Driver");
-        }catch(Exception e){
-            System.out.println("hi1");
         }
-        try (Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/var/run/mysqld/mysqld.sock", "root", "coursework")) {
-            // use con here
+        catch (ClassNotFoundException e)
+        {
+            System.out.println("Could not load SQL driver");
+            System.exit(-1);
         }
-        catch(Exception e){
-            System.out.println("hi2");
+
+        // Connection to the database
+        Connection con = null;
+        int retries = 100;
+        for (int i = 0; i < retries; ++i)
+        {
+            System.out.println("Connecting to database...");
+            try
+            {
+                // Wait a bit for db to start
+                Thread.sleep(30000);
+                // Connect to database
+                con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false", "root", "example");
+                System.out.println("Successfully connected");
+                // Wait a bit
+                Thread.sleep(10000);
+                // Exit for loop
+                break;
+            }
+            catch (SQLException sqle)
+            {
+                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
+                System.out.println(sqle.getMessage());
+            }
+            catch (InterruptedException ie)
+            {
+                System.out.println("Thread interrupted? Should not happen.");
+            }
         }
+
+        if (con != null)
+        {
+            try
+            {
+                // Close connection
+                con.close();
+            }
+            catch (Exception e)
+            {
+                System.out.println("Error closing connection to database");
+            }
+        }
+        //Connect to Database - End
+
         //return;
         int aa = scanner.nextInt();
         //TODO: Add validation for inputs(easy)
