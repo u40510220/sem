458fd42ba0cec7ce4abcac55215e2c749c4cfd01
Juan Alcantara <40488349@live.napier.ac.uk>
2022-04-28 11:35:27 +0100

    Testing added

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,4 +1,5 @@
 //import com.napier.sem.App;
+import com.napier.sem.App;
 import org.junit.jupiter.api.*;
 
 import java.sql.*;
@@ -8,285 +9,199 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 public class MyTest {
-    static Connection con = null;
+    static App app;
+    static String continent = "Europe";
+    static String region = "Polynesia";
+    static String country = "Spain";
+    static String district = "Shanghai";
+    static String city = "Madrid";
+    static int top = 5;
+
+
     @BeforeAll
-    static void prepare(){
-
-        try
-        {
-            // Load Database driver
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        }
-        catch (ClassNotFoundException e)
-        {
-            System.out.println("Could not load SQL driver");
-            System.exit(-1);
-        }
-
-        int retries = 10;
-        for (int i = 0; i < retries; ++i)
-        {
-            System.out.println("Connecting to database...");
-            try
-            {
-                // Wait a bit for db to start
-                Thread.sleep(30000);
-                // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root","example");
-                System.out.println("Successfully connected");
-                break;
-            }
-            catch (SQLException sqle)
-            {
-                System.out.println("Failed to connect to database attempt " + Integer.toString(i)+1);
-                System.out.println(sqle.getMessage());
-            }
-            catch (InterruptedException ie)
-            {
-                System.out.println("Thread interrupted? Should not happen.");
-            }
-        }
+    static void init(){
+        // Connect to database
+        app = new App();
+        app.connect();
     }
     @Test
-    void Q0()
-    {
-        try
-        {
-            // Create an SQL statement
-            Statement stmt = con.createStatement();
-            // Create string for SQL statement
-            String strSelect="Select Name,District " +"From city " + "Order By city.Population Desc";
-            // Execute SQL statement
-            ResultSet rset = stmt.executeQuery(strSelect);
-
-            while (rset.next())
-            {
-                System.out.println(rset.getString("District"));
-            }
-            //return employees;
-
-        }
-        catch (Exception e)
-        {
-            assertEquals(1,0);
-        }
+    void IntegrationDB() {
 
     }
+
     @Test
     void Q1()
     {
-        Q1();
-        assertEquals(5, 5);
+        app.Q1();
     }
     @Test
     void Q2()
     {
-        Q2();
-        assertEquals(5, 5);
+        app.Q2(continent);
+        //TODO test different inputs
     }
     @Test
     void Q3()
     {
-        Q3();
-        assertEquals(5, 5);
+        app.Q3(region);
     }
     @Test
     void Q4()
     {
-        Q4();
-        assertEquals(5, 5);
+        app.Q4(top);
     }
     @Test
     void Q5()
     {
-        Q5();
-        assertEquals(5, 5);
+        app.Q5(continent,top);
     }
     @Test
     void Q6()
     {
-        Q6();
-        assertEquals(5, 5);
+        app.Q6(region,top);
     }
     @Test
     void Q7()
     {
-        Q7();
-        assertEquals(5, 5);
+        app.Q7();
     }@Test
     void Q8()
     {
-        Q8();
-        assertEquals(5, 5);
+        app.Q8(continent);
     }
     @Test
     void Q9()
     {
-        Q9();
-        assertEquals(5, 5);
+        app.Q9(region);
     }
     @Test
     void Q10()
     {
-        Q10();
-        assertEquals(5, 5);
+        app.Q10(country);
     }
     @Test
     void Q11()
     {
-        Q11();
-        assertEquals(5, 5);
+        app.Q11(district);
     }
     @Test
     void Q12()
     {
-        Q12();
-        assertEquals(5, 5);
+        app.Q12(top);
     }
     @Test
     void Q13()
     {
-        Q13();
-        assertEquals(5, 5);
+        app.Q13(continent, top);
     }
     @Test
     void Q14()
     {
-        Q14();
-        assertEquals(5, 5);
+        app.Q14(region, top);
     }
     @Test
     void Q15()
     {
-        Q15();
-        assertEquals(5, 5);
+        app.Q15(country, top);
     }
     @Test
     void Q16()
     {
-        Q16();
-        assertEquals(5, 5);
+        app.Q16(district, top);
     }
     @Test
     void Q17()
     {
-        Q17();
-        assertEquals(5, 5);
+        app.Q17();
     }
     @Test
     void Q18()
     {
-        Q18();
-        assertEquals(5, 5);
+        app.Q18(continent);
     }
     @Test
     void Q19()
     {
-        Q19();
-        assertEquals(5, 5);
+        app.Q19(region);
     }
     @Test
     void Q20()
     {
-        Q20();
-        assertEquals(5, 5);
+        app.Q20(top);
     }
     @Test
     void
     Q21()
     {
-        Q21();
-        assertEquals(5, 5);
+        app.Q21(continent, top);
     }
     @Test
     void Q22()
     {
-        Q22();
-        assertEquals(5, 5);
+        app.Q22(region, top);
     }
     @Test
     void Q23()
     {
-        Q23();
-        assertEquals(5, 5);
+        app.Q23();
     }
     @Test
     void Q24()
     {
-        Q24();
-        assertEquals(5, 5);
+        app.Q24();
     }
     @Test
     void Q25()
     {
-        Q25();
-        assertEquals(5, 5);
+        app.Q25();
     }
     @Test
     void Q31()
     {
-        Q31();
-        assertEquals(5, 5);
+        app.Q31();
     }
     @Test
     void Q32()
     {
-        Q32();
-        assertEquals(5, 5);
+        app.Q32(continent);
     }
     @Test
     void Q33()
     {
-        Q33();
-        assertEquals(5, 5);
+        app.Q33(region);
     }
     @Test
     void Q34()
     {
-        Q34();
-        assertEquals(5, 5);
+        app.Q34(country);
     }
     @Test
     void Q35()
     {
-        Q35();
-        assertEquals(5, 5);
+        app.Q35(district);
     }
     @Test
     void Q36()
     {
-        Q36();
-        assertEquals(5, 5);
+        app.Q36(city);
     }
     @Test
     void Q37()
     {
-        Q37();
-        assertEquals(5, 5);
+        app.Q37();
     }
 
 
     @Test
     void connect(){
-        connect();
+        app.connect();
         //if connection successful useless assertEquals confirms it
-        assertEquals(1,1);
     }
 
     @AfterAll
-    static void finalise(){
-        if (con != null)
-        {
-            try
-            {
-                // Close connection
-                con.close();
-            }
-            catch (Exception e)
-            {
-                System.out.println("Error closing connection to database");
-            }
-        }
+    static void finalise() {
+        app.disconnect();
+
     }
 
 }
